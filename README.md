62b66bccf3dfdd7a82057dcf1d17cd5b904f9408
portfolio <ankitmaharana600@gmail.com>
2025-07-23 19:51:36 +0530

    Doen changes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Demo-aI-
 This is demo repo for checking how the web is working
+To those who want to use this demo file:
+Here in the backend you have to create a .env file for the web site to work
--- backend/app/main.py
+++ backend/app/main.py
@@ -45,7 +45,7 @@ def create_access_token(data: dict, expires_delta: datetime.timedelta = None):
     to_encode = data.copy()
     expire = datetime.datetime.utcnow() + (expires_delta or datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM) # type: ignore
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
@@ -54,7 +54,7 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM]) # type: ignore
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
